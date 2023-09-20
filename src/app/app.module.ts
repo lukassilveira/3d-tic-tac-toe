da514da33b56df04d27b60ed22949118506abcba
Lukas Silveira <lukassilveira12@hotmail.com>
2023-09-19 22:38:26 -0300

    creating service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 
 import { FormsModule } from '@angular/forms';
 
@@ -20,6 +21,7 @@ const config: SocketIoConfig = { url: 'http://localhost:3000', options: {
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     SocketIoModule.forRoot(config),
     FormsModule
   ],
